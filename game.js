b1f811c2900611dad58567bedf4bf91337ba46dd
Nikol <nikolhurd@gmail.com>
2025-02-20 17:13:00 +0100

    add other movements

--- game.js
+++ game.js
@@ -47,10 +47,29 @@ const borderCheckUp = (newY, newX) => {
 // movement on map - where is Player on the map(coordinates) where Player already been(variable visited)
 
 const moveUp = () => {
-  grid[playerY][playerX] = 3;
-  playerY -= 1;
-  grid[playerY][playerX] = 1;
+  grid[playerY][playerX] = 3; // where player currently is
+  playerY -= 1; // moving up the coordinates
+  grid[playerY][playerX] = 1; // where player is after moving up
 };
+
+const moveDown = () => {
+  grid[playerY][playerX] = 3; // where player currently is
+  playerY += 1; // moving down
+  grid[playerY][playerX] = 1; // where player is after moving down
+};
+
+const moveRight = () => {
+  grid[playerY][playerX] = 3; // where player currently is
+  playerX += 1; // moving right
+  grid[playerY][playerX] = 1; // where player is after moving right
+};
+
+const moveLeft = () => {
+  grid[playerY][playerX] = 3; // where player currently is
+  playerX -= 1; //moving left
+  grid[playerY][playerX] = 1; // where player is after moving left
+};
+
 const processMove = () => {
   const newY = playerY - 1;
   if (borderCheckUp(newY, playerX)) {
@@ -65,7 +84,6 @@ const cannotMove = () => {
 };
 
 processMove();
-console.log(playerY);
+processMove();
 showGrid();
-
 // inquirer for the users input
