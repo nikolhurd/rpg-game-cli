64c32add91221e069eaa3ffe3ce0b1972ab000c7
Nikol <nikolhurd@gmail.com>
2025-02-20 12:38:11 +0100

    Initial game structure design and basic concepts

---
+++ game.js
@@ -0,0 +1,19 @@
+// main class Entity with hp, ap, dp, sprite
+
+// CombatEntity extends Entity
+// fight()
+
+// Player extends CombatEntity
+// move()
+
+// Enemy extends CombatEntity
+
+// Item extends Entity
+// applyToPlayer()
+
+// function createGrid(height, width) - array
+
+// movement on map - where is Player on the map(coordinates), where are the borders, where Player already been(variable visited)
+// start - left bottom
+// end - right up
+// inquirer for the users input
