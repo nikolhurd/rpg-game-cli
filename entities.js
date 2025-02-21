c6c8a3ec4758568ed484eeb5286f81ba88e02576
Nikol <nikolhurd@gmail.com>
2025-02-21 11:00:20 +0100

    more constructors, fight function

--- entities.js
+++ entities.js
@@ -8,13 +8,35 @@ class Entity {
 }
 
 class CombatEntity extends Entity {
-  // fight()
+  constructor(hp, ap, dp) {
+    super(hp, ap, dp);
+  }
+
+  fight(oponent) {
+    while (oponent.hp > 0 && this.hp > 0) {
+      let damageToOponent = this.ap - oponent.dp;
+      console.log("Oponent HP:", oponent.hp, "This HP:", this.hp);
+      let damageToThis = oponent.ap - this.dp;
+
+      oponent.hp -= damageToOponent;
+      this.hp -= damageToThis;
+    }
+    console.log("Oponent health", oponent.hp);
+    console.log("This health", this.hp);
+  }
 }
 
 class Player extends CombatEntity {
-  // move()
+  constructor(hp, ap, dp) {
+    super(hp, ap, dp);
+    // move()
+  }
 }
-class Enemy extends CombatEntity {}
+class Enemy extends CombatEntity {
+  constructor(hp, ap, dp) {
+    super(hp, ap, dp);
+  }
+}
 
 class Item extends Entity {
   // applyToPlayer()
